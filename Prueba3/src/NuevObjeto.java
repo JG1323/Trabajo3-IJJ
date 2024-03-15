b6110f1155f52da79c114c34c041e5726b173f7b
Kokushibou97 <136746018+Kokushibou97@users.noreply.github.com>
2024-03-15 13:11:32 +0100

    commit prueba

---
+++ Prueba3/src/NuevObjeto.java
@@ -0,0 +1,4 @@
+
+public class NuevObjeto {
+	public int edad=30;
+}
---
+++ Prueba3/src/NuevObjetoApp.java
@@ -0,0 +1,12 @@
+
+public class NuevObjetoApp {
+
+	public static void main(String[] args) {
+
+		NuevObjeto uno = new NuevObjeto();
+		
+		System.out.println("La edad del objeto uno es " + uno.edad);
+		
+	}
+
+}
